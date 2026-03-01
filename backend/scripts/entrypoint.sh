2c95c7c77649a2786addde291f808cf740fb9147
Himanshu <negihimanshu015@gmail.com>
2026-03-01 10:47:02 +0530

    refact: added static file in  script

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -3,6 +3,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+echo "Collecting static files..."
+python backend/manage.py collectstatic --noinput
+
 echo "Running migrations..."
 python backend/manage.py migrate --noinput
 
